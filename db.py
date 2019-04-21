ebb46f827698098cbfb4799c758c9f8b7d9d22df
samwalton9 <samwalton9@gmail.com>
2019-04-21 21:19:13 +0100

    Add database code and schema

--- db.py
+++ db.py
@@ -1,24 +1,55 @@
-#import mysqlclient # TODO: Install
+import MySQLdb
+import os
+
+db = MySQLdb.connect(host="tools.db.svc.eqiad.wmflabs",
+                     db='s54021__retractionbot',
+                     read_default_file=os.path.expanduser("~/replica.my.cnf"))
 
 
-def save_retraction_to_db(type, original_id, retraction_id):
+def save_retraction_to_db(type, origin, original_id, retraction_id):
     """
-    Given a certain type of identifier (e.g. doi, pmid), and both the
-    new (retraction) id and old (retracted) id, save this to the DB.
+    Given a certain type of identifier (e.g. doi, pmid), its origin (e.g. crossref, pubmed)
+    and both the new (retraction) id and old (retracted) id, save this to the DB.
+    type can be 'doi' or 'pmid'
     """
-    pass
+    cur = db.cursor()
+    query = """
+        INSERT INTO retractions
+        VALUES ('{type}', '{origin}', '{original_id}', '{retraction_id}')"""
+    cur.execute(query.format(
+        type=type,
+        origin=origin,
+        original_id=original_id,
+        retraction_id=retraction_id
+    ))
 
 
 def check_retracted_ids(identifier):
     current_ids = load_retracted_identifiers()
-    for db_id in current_ids:
-        if db_id[1] == identifier:
-            return False
+    # TODO: Check if this ID is already in the DB.
 
     return True
 
 
 def load_retracted_identifiers():
-    # This return is purely placeholder for testing.
-    # Returns identifier type, retraction DOI, original DOI
-    return [['doi', '10.783947932473298/retracted', '10.783947932473298/dth.12107']]
+    cur = db.cursor()
+    query = """
+        SELECT * FROM retractions
+    """
+    cur.execute(query)
+    return list(cur.fetchall())
+
+
+def log_retraction_edit(timestamp, domain, page_title, old_id, new_id):
+    cur = db.cursor()
+    query = """
+        INSERT INTO edit_log
+        VALUES ('{timestamp}', '{domain}', '{page_title}', '{old_id}', '{new_id}')
+    """
+    cur.execute(query.format(
+        timestamp=timestamp,
+        domain=domain,
+        page_title=page_title,
+        old_id=old_id,
+        new_id=new_id
+    ))
--- retractionbot.py
+++ retractionbot.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
+import datetime
 import logging
+import os
 import pywikibot
 from pywikibot import pagegenerators
-import os
 import re
-import datetime
 import yaml
 
-from db import load_retracted_identifiers
+from db import load_retracted_identifiers, log_retraction_edit
 
 directory = os.path.dirname(os.path.realpath(__file__))
 
@@ -40,7 +40,7 @@ def check_bot_killswitches(site):
             logger.error(log_text)
             return False
 
-    # If we haven't returned False yet then everything
+    # If we haven't returned False then everything
     # seems to be fine, so return True.
     return True
 
@@ -58,9 +58,8 @@ def find_page_cites(page_text, id):
     that identifier. Returns a list.
     """
     soup = BeautifulSoup(page_text, 'html.parser')
-    page_cites = soup.find_all("ref", string=re.compile('.*{id}.*'.format(
-        id=id
-    )))
+    escaped_id = re.escape(id)
+    page_cites = soup.find_all("ref", string=re.compile(escaped_id, re.IGNORECASE))
     return page_cites
 
 
@@ -80,26 +79,27 @@ def run_bot():
             continue
 
         for identifier in retracted_identifiers:
+            original_id = identifier[2].decode("utf-8")
+            retraction_id = identifier[3].decode("utf-8")
             retracted_template = template_template.format(
                 template_name=lang_items,
-                id_field=template_field_names[identifier[0]],
-                id=identifier[1])
+                id_field=template_field_names[identifier[0].decode("utf-8")],
+                id=retraction_id)
 
             # TODO: Turn back to 0 when not testing in sandbox
-            page_list = pagegenerators.SearchPageGenerator(identifier[2],
+            page_list = pagegenerators.SearchPageGenerator('"' + original_id + '"',
                                                            namespaces=[2],
                                                            site=site)
 
             for wp_page in page_list:
-                print(wp_page)
                 page_text = wp_page.text
 
-                page_cites = find_page_cites(page_text, identifier[2])
+                page_cites = find_page_cites(page_text, original_id)
                 num_cites_found = len(page_cites)
                 if num_cites_found == 0:
-                    logger.error("Error: Couldn't find the identifier {id} inside"
+                    logger.error("Couldn't find the identifier {id} inside "
                                  "<ref> tags on page {page}.".format(
-                                    id=identifier[2],
+                                    id=original_id,
                                     page=wp_page))
                     continue
 
@@ -123,11 +123,16 @@ def run_bot():
                     wp_page.text = page_text
                     edit_summary = "Flagging a cited source as retracted"
 
-                    wp_page.save(edit_summary, minor=False)
+                    #wp_page.save(edit_summary, minor=False)
                     logger.info("Successfully edited {page_name} with "
                                 "retracted source(s).".format(
                                     page_name=wp_page.title()
                                 ))
+                    log_retraction_edit(datetime.datetime.now(),
+                                        language + ".wikipedia.org",
+                                        wp_page,
+                                        original_id,
+                                        retraction_id)
 
 
 if __name__ == '__main__':
