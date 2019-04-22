1fea2af9c330cfe8b91c62369f6dd796a7d53b1c
samwalton9 <samwalton9@gmail.com>
2019-04-22 10:51:50 +0100

    Fix col length bug

--- schema.sql
+++ schema.sql
@@ -2,14 +2,14 @@ CREATE TABLE `retractions` (
   `timestamp` TIMESTAMP NOT NULL,
   `type` varbinary(10) NOT NULL,
   `origin` varbinary(20) NOT NULL,
-  `original_id` varbinary(50) NOT NULL,
-  `retraction_id` varbinary(50) NOT NULL
+  `original_id` varbinary(200) NOT NULL,
+  `retraction_id` varbinary(200) NOT NULL
 ) ENGINE=Aria;
 
 CREATE TABLE `edit_log` (
   `timestamp` TIMESTAMP NOT NULL,
   `domain` varbinary(20) NOT NULL,
   `page_title` varbinary(255) NOT NULL,
-  `original_id` varbinary(50) NOT NULL,
-  `retraction_id` varbinary(50) NOT NULL
+  `original_id` varbinary(200) NOT NULL,
+  `retraction_id` varbinary(200) NOT NULL
 ) ENGINE=Aria;
